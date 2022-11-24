d948e67aa743e608c0e19fa9b780d232d9743044
JLucasXavier <lucasxaviert875@gmail.com>
2022-11-24 11:03:53 -0300

    Página inicial do site

--- ReservaChurras/ReservaChurras/urls.py
+++ ReservaChurras/ReservaChurras/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Reservas import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='homepage'),
+    path('SegundaPagina', views.segundaPagina,
+        name='segunda'),
 ]
